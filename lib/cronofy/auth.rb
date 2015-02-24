626047ea42770d9ffab03b40d8335f211509a022
Adam Bird <adam.bird@gmail.com>
2015-02-24 10:15:41 +0000

    moved Credentials

--- lib/cronofy/auth.rb
+++ lib/cronofy/auth.rb
@@ -5,6 +5,30 @@ module Cronofy
     APP_URL = 'https://app.cronofy.com'
     API_URL = 'https://api.cronofy.com'
 
+    class Credentials
+
+      attr_reader :access_token,
+                  :expires_at,
+                  :expires_in,
+                  :refresh_token
+
+      def initialize(oauth_token)
+        @access_token = oauth_token.token
+        @expires_at = oauth_token.expires_at
+        @expires_in = oauth_token.expires_in
+        @refresh_token = oauth_token.refresh_token
+      end
+
+      def to_hash
+        {
+          access_token: access_token,
+          refresh_token: refresh_token,
+          expires_in: expires_in,
+          expires_at: expires_at
+        }
+      end
+    end
+
     attr_reader :access_token
 
     def initialize(client_id, client_secret, token=nil, refresh_token=nil)
@@ -50,28 +74,5 @@ module Cronofy
       @access_token = OAuth2::AccessToken.new(@api_client, token, { refresh_token: refresh_token })
     end
 
-    class Credentials
-
-      attr_reader :access_token,
-                  :expires_at,
-                  :expires_in,
-                  :refresh_token
-
-      def initialize(oauth_token)
-        @access_token = oauth_token.token
-        @expires_at = oauth_token.expires_at
-        @expires_in = oauth_token.expires_in
-        @refresh_token = oauth_token.refresh_token
-      end
-
-      def to_hash
-        {
-          access_token: access_token,
-          refresh_token: refresh_token,
-          expires_in: expires_in,
-          expires_at: expires_at
-        }
-      end
-    end
   end
 end
