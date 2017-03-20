70be29f473e2d563bfd0cf17f8fb15b991d64d3b
Harry Boyes <harry.boyes@gmail.com>
2017-03-20 09:43:25 +0000

    Add description for upsert attendees

--- lib/cronofy/client.rb
+++ lib/cronofy/client.rb
@@ -120,6 +120,10 @@ module Cronofy
     #                                          occur.
     #               :transparency - The transparency state for the event (optional).
     #                               Accepted values are "transparent" and "opaque".
+    #               :attendees    - A Hash of :invite and :reject, each of which is
+    #                               an array of invitees to invite to or reject from
+    #                               the event. Invitees are represented by a hash of
+    #                               :email and :display_name (optional).
     #
     # Examples
     #
